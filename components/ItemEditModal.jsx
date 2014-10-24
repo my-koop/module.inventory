74b46f3650e267b6799dd6cf5822ad895fd61782
Cellule <mike.ferris@hotmail.com>
2014-10-24 18:18:28 -0400

    fix indentation

--- components/ItemEditModal.jsx
+++ components/ItemEditModal.jsx
@@ -9,10 +9,10 @@ var ItemEditModal = React.createClass({
   render: function () {
     return this.transferPropsTo(
       <BSModal title={"Editing item " + this.props.name} backdrop="static">
-        <div className="modal-body" > 
+        <div className="modal-body" >
           <BSInput type="static" label="ID" placeholder="ID" value={this.props.itemId} />
-            <BSInput type="text" label="Item Name" placeholder="Name" defaultValue={this.props.name} />
-            <BSInput type="text" label="Price" placeholder="Price" />
+          <BSInput type="text" label="Item Name" placeholder="Name" defaultValue={this.props.name} />
+          <BSInput type="text" label="Price" placeholder="Price" />
         </div>
         <div className="modal-footer">
           <BSButton onClick={this.props.onRequestHide}>Close</BSButton>
