9481e74971f8c8b96f3f723e22f49a0d5352c09a
Cellule <mike.ferris@hotmail.com>
2014-10-31 01:23:58 -0400

    fixed validation on update item

--- lib/validation/index.ts
+++ lib/validation/index.ts
@@ -3,8 +3,8 @@ import er = require("../classes/endpointsResponses");
 var Validator = require("jsonschema").Validator;
 var validator = new Validator();
 
-function validate(obj, schema: {id: string}) {
-  var validationResult = validator.validate(obj, {"$ref": schema.id}).errors;
+function validate(obj, schema) {
+  var validationResult = validator.validate(obj, schema).errors;
   return _.isEmpty(validationResult) ?
     null
     : validationResult.map(function(res) {
@@ -16,6 +16,7 @@ var updateItemSchema = {
   "id": "/UpdateItem",
   "type": "object",
   "properties": {
+    "id": {"type": "number", "required": true},
     "code": {"type": "number", "required": true},
     "name": {"type": "string", "required": true},
     "price": {"type": "number", "required": true}
